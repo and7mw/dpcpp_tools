3d33eb779cc720bcb831fab22f4e4323bbe1b1a3
and7mw <and7mw@gmail.com>
2022-12-01 23:42:04 +0300

    investig 1

--- debug_tool/src/debug_tool.cpp
+++ debug_tool/src/debug_tool.cpp
@@ -20,6 +20,8 @@
 #include <atomic>
 #include <sstream>
 
+#include "CL/sycl/detail/pi.h"
+
 namespace xpti {
 class ThreadID {
 public:
@@ -125,47 +127,6 @@ XPTI_CALLBACK_API void xptiTraceInit(unsigned int major_version,
     // been registered by the framework and will return the previously
     // registered stream ID
     GStreamID = xptiRegisterStream(stream_name);
-    // Register our lone callback to all pre-defined trace point types
-    xptiRegisterCallback(GStreamID,
-                         (uint16_t)xpti::trace_point_type_t::graph_create,
-                         tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::node_create, tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::edge_create, tpCallback);
-    xptiRegisterCallback(GStreamID,
-                         (uint16_t)xpti::trace_point_type_t::region_begin,
-                         tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::region_end, tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::task_begin, tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::task_end, tpCallback);
-    xptiRegisterCallback(GStreamID,
-                         (uint16_t)xpti::trace_point_type_t::barrier_begin,
-                         tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::barrier_end, tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::lock_begin, tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::lock_end, tpCallback);
-    xptiRegisterCallback(GStreamID,
-                         (uint16_t)xpti::trace_point_type_t::transfer_begin,
-                         tpCallback);
-    xptiRegisterCallback(GStreamID,
-                         (uint16_t)xpti::trace_point_type_t::transfer_end,
-                         tpCallback);
-    xptiRegisterCallback(GStreamID,
-                         (uint16_t)xpti::trace_point_type_t::thread_begin,
-                         tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::thread_end, tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::wait_begin, tpCallback);
-    xptiRegisterCallback(
-        GStreamID, (uint16_t)xpti::trace_point_type_t::wait_end, tpCallback);
     xptiRegisterCallback(GStreamID, (uint16_t)xpti::trace_point_type_t::signal,
                          tpCallback);
     printf("Registered all callbacks\n");
@@ -185,8 +146,30 @@ std::string truncate(std::string Name) {
   }
 }
 
+_pi_plugin* plg;
+_pi_event * pi_ev;
+
 XPTI_CALLBACK_API void xptiTraceFinish(const char *stream_name) {
-  // We do nothing here
+  std::cout << "xptiTraceFinish" << std::endl;
+    if (plg != nullptr) {
+      uint64_t time1 = 123;
+      auto ret = (*plg->PiFunctionTable.piEventGetProfilingInfo)(
+        pi_ev,
+        _pi_profiling_info::PI_PROFILING_INFO_COMMAND_START,
+        sizeof(uint64_t),
+        &time1,
+        nullptr
+      );
+      uint64_t time2 = 123;
+      ret = (*plg->PiFunctionTable.piEventGetProfilingInfo)(
+        pi_ev,
+        _pi_profiling_info::PI_PROFILING_INFO_COMMAND_END,
+        sizeof(uint64_t),
+        &time2,
+        nullptr
+      );
+      std::cout << ret << " : " << time2 << " " << time1 << std::endl;
+    }
 }
 
 XPTI_CALLBACK_API void tpCallback(uint16_t TraceType,
@@ -194,38 +177,63 @@ XPTI_CALLBACK_API void tpCallback(uint16_t TraceType,
                                   xpti::trace_event_data_t *Event,
                                   uint64_t Instance, const void *UserData) {
   auto Payload = xptiQueryPayload(Event);
-  xpti::timer::tick_t Time = xpti::timer::rdtsc();
-  auto TID = xpti::timer::getThreadID();
-  uint32_t CPU = GThreadIDEnum.enumID(TID);
-  std::string Name;
 
+  std::string Name;
   if (Payload->name_sid() != xpti::invalid_id) {
     Name = truncate(Payload->name);
   } else {
     Name = "<unknown>";
   }
+  
+  if (Name == "Sub") {
+    plg = static_cast<_pi_plugin*>(Event->global_user_data);
+    pi_ev = static_cast<_pi_event *>(const_cast<void *>(UserData));
 
-  uint64_t ID = Event ? Event->unique_id : 0;
-  // Lock while we print information
-  std::lock_guard<std::mutex> Lock(GIOMutex);
-  // Print the record information
-  if (UserData != nullptr) {
-    std::cout << std::string(reinterpret_cast<const char *>(UserData)) << std::endl;
-  }
-  printf("%-25lu: name=%-35s cpu=%3d event_id=%10lu\n", Time, Name.c_str(), CPU,
-         ID);
-  // Go through all available meta-data for an event and print it out
-  xpti::metadata_t *Metadata = xptiQueryMetadata(Event);
-  for (const auto &Item : *Metadata) {
-    std::cout << "     ";
-    std::cout << xptiLookupString(Item.first) << " : ";
-    std::cout << xpti::readMetadata(Item) << "\n";
+std::cout << "ASSIGN KERNEL : " << pi_ev << std::endl;
+
+    uint64_t time1 = 123;
+      auto ret = (*plg->PiFunctionTable.piEventGetProfilingInfo)(
+        pi_ev,
+        _pi_profiling_info::PI_PROFILING_INFO_COMMAND_START,
+        sizeof(uint64_t),
+        &time1,
+        nullptr
+      );
+      uint64_t time2 = 123;
+      ret = (*plg->PiFunctionTable.piEventGetProfilingInfo)(
+        pi_ev,
+        _pi_profiling_info::PI_PROFILING_INFO_COMMAND_END,
+        sizeof(uint64_t),
+        &time2,
+        nullptr
+      );
+      std::cout << ret << " : " << time2 << " " << time1 << std::endl;
   }
 
-  if (Payload->source_file_sid() != xpti::invalid_id && Payload->line_no > 0) {
-    printf("---[Source file:line no] %s:%d\n", Payload->source_file,
-           Payload->line_no);
-  }
+  std::cout << "tpCallback end!" << std::endl;
+}
+
+__attribute__((destructor)) static void fwFinialize() {
+    std::cout << "DESTROY" << std::endl;
+    if (plg != nullptr) {
+      uint64_t time1 = 123;
+      auto ret = (*plg->PiFunctionTable.piEventGetProfilingInfo)(
+        pi_ev,
+        _pi_profiling_info::PI_PROFILING_INFO_COMMAND_START,
+        sizeof(uint64_t),
+        &time1,
+        nullptr
+      );
+      uint64_t time2 = 123;
+      ret = (*plg->PiFunctionTable.piEventGetProfilingInfo)(
+        pi_ev,
+        _pi_profiling_info::PI_PROFILING_INFO_COMMAND_END,
+        sizeof(uint64_t),
+        &time2,
+        nullptr
+      );
+      std::cout << ret << " : " << time2 << " " << time1 << std::endl;
+    }
 }
 
 #if (defined(_WIN32) || defined(_WIN64))
@@ -251,12 +259,12 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fwdReason, LPVOID lpvReserved) {
 
 #else // Linux (possibly macOS?)
 
-__attribute__((constructor)) static void framework_init() {
-  // printf("Framework initialization\n");
-}
+// __attribute__((constructor)) static void framework_init() {
+//   // printf("Framework initialization\n");
+// }
 
-__attribute__((destructor)) static void framework_fini() {
-  // printf("Framework finalization\n");
-}
+// __attribute__((destructor)) static void framework_fini() {
+//   // printf("Framework finalization\n");
+// }
 
 #endif
--- samples/single_vector_op.cpp
+++ samples/single_vector_op.cpp
@@ -11,7 +11,8 @@ int main(int argc, char* argv[]) {
         int A_val = 1, B_val = 2;
         std::vector<int> A(size, A_val), B(size, B_val), result(size);
         {
-        sycl::queue queue{*selector};
+        sycl::property_list prop_list{sycl::property::queue::enable_profiling()};
+        sycl::queue queue{*selector, sycl::async_handler{}, prop_list};
 
         std::cout << "Target device: "
               << queue.get_info<sycl::info::queue::device>().get_info<sycl::info::device::name>() << std::endl;
@@ -23,7 +24,7 @@ int main(int argc, char* argv[]) {
         sycl::buffer<int, 1> result_buffer(result.data(), result.size());
 
         for (size_t i = 0; i < iter_num; i++) {
-            queue.submit([&](sycl::handler &cgh) {
+            sycl::event event = queue.submit([&](sycl::handler &cgh) {
                 auto A_acc = A_buffer.get_access<sycl::access::mode::read>(cgh);
                 auto B_acc = B_buffer.get_access<sycl::access::mode::read>(cgh);
 
@@ -35,7 +36,13 @@ int main(int argc, char* argv[]) {
 
             });
             queue.wait_and_throw();
+
+            auto start = event.get_profiling_info<sycl::info::event_profiling::command_start>();
+            auto end = event.get_profiling_info<sycl::info::event_profiling::command_end>();
+            auto time = static_cast<double>(end - start);
+            std::cout << "Time: " << time <<" sec" << std::endl;
         }
+
         }
         for (const auto& r : result) {
             if (r != (A_val - B_val)) {
