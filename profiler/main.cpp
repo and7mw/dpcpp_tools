4b63b8458318893ff631a9c11f1bed541c0f2800
and7mw <and7mw@gmail.com>
2023-04-25 21:19:51 +0300

    added runners for tools

--- profiler/main.cpp
+++ profiler/main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include "../utils/runner_utils.h"
 
-int main() {
-    std::cout << "HELLO" << std::endl;
+int main(int argc, char **argv, char **env) {
+#ifdef __linux__
+    if (argc <= 1) {
+        std::cout << "Provide path to app for analysis!" << std::endl;
+        return 1;
+    }
+
+    std::vector<char *> new_env;
+    size_t i = 0;
+    while (env[i] != nullptr) {
+        new_env.push_back(env[i++]);
+    }
+
+    std::string selfPath = getPathToRunner();
+    const auto pos = selfPath.find_last_of("/");
+    selfPath = selfPath.substr(0, pos);
+
+    std::string trace_enable_env = "XPTI_TRACE_ENABLE=1";
+    std::string xptifw_env = "XPTI_FRAMEWORK_DISPATCHER=libxptifw.so";
+    std::string subscr_env = "XPTI_SUBSCRIBERS=" + selfPath + "/libprofiler_lib.so";
+    new_env.push_back(const_cast<char *>(trace_enable_env.data()));
+    new_env.push_back(const_cast<char *>(xptifw_env.data()));
+    new_env.push_back(const_cast<char *>(subscr_env.data()));
+    new_env.push_back(nullptr);
+
+    if (execve(argv[1], argv + 1, new_env.data())) {
+        std::cout << "Can't launch target app!" << std::endl;
+        return 1;
+    }
+#else
+    throw std::runtime_error("Runner doesn't work for this OS, set"
+                             " XPTI_TRACE_ENABLE, XPTI_FRAMEWORK_DISPATCHER, XPTI_SUBSCRIBERS manually!");
+#endif
     return 0;
 }
---
+++ utils/runner_utils.h
@@ -0,0 +1,32 @@
+#ifdef __linux__
+#include <unistd.h>
+#include <sys/syscall.h>
+#endif
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+inline std::string getPathToRunner() {
+    std::string runnerPath;
+#ifdef __linux__
+    char buffer[1024] = { 0 };
+    ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
+    if (len <= 0) {
+        throw std::runtime_error("readlink failed!");
+    } else {
+        buffer[len] = '\0';
+        runnerPath = std::string(buffer);
+    }
+#else
+    throw std::runtime_error("Can't get path to runner on this OS!");
+#endif
+    return runnerPath;
+}
+
+inline std::vector<std::string> getArgs(int argc, char **argv) {
+    std::vector<std::string> args;
+    
+
+    return args;
+}
