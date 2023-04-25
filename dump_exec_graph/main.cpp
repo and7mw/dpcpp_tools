ee879f1d08d00511d05a86821ca5eceddbd1acc9
and7mw <and7mw@gmail.com>
2023-04-25 21:34:24 +0300

    profiler -> hotspots

--- dump_exec_graph/main.cpp
+++ dump_exec_graph/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char **argv, char **env) {
 
     std::string trace_enable_env = "XPTI_TRACE_ENABLE=1";
     std::string xptifw_env = "XPTI_FRAMEWORK_DISPATCHER=libxptifw.so";
-    std::string subscr_env = "XPTI_SUBSCRIBERS=" + selfPath + "/libdump_exec_graph_lib.so";
+    std::string subscr_env = "XPTI_SUBSCRIBERS=" + selfPath + "/libdump_exec_graph_sub.so";
     new_env.push_back(const_cast<char *>(trace_enable_env.data()));
     new_env.push_back(const_cast<char *>(xptifw_env.data()));
     new_env.push_back(const_cast<char *>(subscr_env.data()));
